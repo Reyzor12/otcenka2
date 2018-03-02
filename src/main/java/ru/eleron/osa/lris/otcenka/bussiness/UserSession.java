d056a55ff9a74a77c48439a9772fc8f2dc944abb
reyzor <testaccwow001@gmail.com>
2018-03-02 16:00:55 +0300

    almost all TODO report WORD in head of head

--- src/main/java/ru/eleron/osa/lris/otcenka/bussiness/UserSession.java
+++ src/main/java/ru/eleron/osa/lris/otcenka/bussiness/UserSession.java
@@ -6,14 +6,17 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.eleron.osa.lris.otcenka.entities.*;
+import ru.eleron.osa.lris.otcenka.service.dao.BaseOperationIF;
 import ru.eleron.osa.lris.otcenka.service.dao.OpenReportDao;
 import ru.eleron.osa.lris.otcenka.service.dao.UserDao;
 import ru.eleron.osa.lris.otcenka.utilities.MessageGenerator;
 import ru.eleron.osa.lris.otcenka.utilities.SceneLoader;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * UserSession class is kind of cache of user session
@@ -31,41 +34,35 @@ public class UserSession {
 
     @Autowired
     private MessageGenerator messageGenerator;
-
     @Autowired
     private OpenReportDao<OpenReport> openReportDao;
-
     @Autowired
     private UserDao<User> userDao;
+    @Autowired
+    private BaseOperationIF<ReportYear> baseOperationReportYear;
+    @Autowired
+    private BaseOperationIF<Department> baseOperationDepartment;
 
     private LocalDate serverDate;
-
     private ComputerName computerName;
-
     private User user;
-
     private List<OpenReport> openReportList;
-
     private OpenReport choosenOpenReport;
-
     private List<ReportYear> reportYearList;
-
     private List<User> usersOfDepartment;
-
     private Report report;
+    private Map<ReportYear,Map<Integer,Map<Department,List<Integer>>>> allDepartmentsStatistic;
+    private List<Department> departmentList;
 
     public ComputerName getComputerName() {
         return computerName;
     }
-
     public void setComputerName(ComputerName computerName) {
         this.computerName = computerName;
     }
-
     public User getUser(){
         return user;
     }
-
     public void setUser(User user){
         this.user = user;
     }
@@ -80,27 +77,22 @@ public class UserSession {
     public void setOpenreportList(List<OpenReport> openReportList) {
         this.openReportList = openReportList;
     }
-
     public LocalDate getServerDate() {
         return serverDate;
     }
-
     public void setServerDate(LocalDate serverDate) {
         this.serverDate = serverDate;
     }
-
     public OpenReport getChoosenOpenReport() {
         return choosenOpenReport;
     }
-
     public void setChoosenOpenReport(OpenReport choosenOpenReport) {
         this.choosenOpenReport = choosenOpenReport;
     }
-
     public List<ReportYear> getReportYearList() {
+        if(reportYearList == null) reportYearList = baseOperationReportYear.getList();
         return reportYearList;
     }
-
     public void setReportYearList(List<ReportYear> reportYearList) {
         this.reportYearList = reportYearList;
     }
@@ -117,11 +109,67 @@ public class UserSession {
     }
 
     public ReportYear getCurrentReportYear(Integer year){
-        ReportYear y1 = new ReportYear(year);
         for(ReportYear y: getReportYearList()){
-            if(y.equals(y1)) return y;
+            if(y.getYear().equals(year)) return y;
         }
         return null;
+    }
+
+    public List<Department> getDepartmentList() {
+        if(departmentList == null) this.departmentList = baseOperationDepartment.getList();
+        return departmentList;
+    }
+
+    public void setDepartmentList(List<Department> departmentList) {
+        this.departmentList = departmentList;
+    }
+
+    /**
+     * Get List of departments statistic for set reportYear and month
+     *
+     * @param reportYear - statistic for this year
+     * @param month - statistic for this month
+     * @return - list of classes of  department's statistic
+     * */
+
+    public Map<Department,List<Integer>> getDepartmentStatisticForDate(ReportYear reportYear, Integer month) {
+        if (allDepartmentsStatistic == null) initAllDepartmentsStatistic();
+        Map<Department,List<Integer>> departmentStatisticMap = allDepartmentsStatistic.get(reportYear).get(month);
+        if (departmentStatisticMap == null) {
+            departmentStatisticMap = new HashMap<>();
+            for(Department department : getDepartmentList()) {
+                departmentStatisticMap.put(department, Arrays.asList(0,0));
+            }
+            List<OpenReport> openReportList = openReportDao.getListWithDepartments(reportYear, month);
+            for (OpenReport openReport : openReportList) {
+                List<Integer> list = departmentStatisticMap.get(openReport.getReport().getDepartment());
+                Integer i = list.get(1);
+                i += 1;
+                list.set(1,i);
+                if (openReport.getStatus().equals(OpenReport.REPORT_APPROVED)) {
+                    Integer j = list.get(0);
+                    j += 1;
+                    list.set(0,j);
+                }
+            }
+            allDepartmentsStatistic.get(reportYear).put(month,departmentStatisticMap);
+        }
+        return departmentStatisticMap;
+    }
+
+    /**
+     * Init parameter allDepartmentsStatistic
+     * */
+
+    private void initAllDepartmentsStatistic() {
+        allDepartmentsStatistic = new HashMap<>();
+        for(ReportYear year : reportYearList) {
+            Map<Integer,Map<Department,List<Integer>>> map = new HashMap<>();
+            for(int i = 1; i < 13; i++) {
+                map.put(i,null);
+            }
+            allDepartmentsStatistic.put(year,map);
+        }
     }
 
     public void toMainFrame(ActionEvent event){
--- src/main/java/ru/eleron/osa/lris/otcenka/controllers/RoleNiokrHeadMainFrameController.java
+++ src/main/java/ru/eleron/osa/lris/otcenka/controllers/RoleNiokrHeadMainFrameController.java
@@ -1,11 +1,109 @@
 package ru.eleron.osa.lris.otcenka.controllers;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.eleron.osa.lris.otcenka.bussiness.UserSession;
+import ru.eleron.osa.lris.otcenka.entities.Department;
+import ru.eleron.osa.lris.otcenka.entities.ReportYear;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Controller for RoleNiokrHeadMainFrame.fxml
+ *
+ * @author reyzor
+ * @version 1.0
+ * @since 02.03.2018
+ * */
 
 @Component
 public class RoleNiokrHeadMainFrameController {
 
-    public void initialize() {
+    @Autowired
+    private UserSession userSession;
 
+    @FXML private ChoiceBox<ReportYear> choiceBoxYear;
+    @FXML private ChoiceBox<Integer> choiceBoxMonth;
+    @FXML private TableView<Department> tableViewDepartment;
+    @FXML private TableColumn<Department,String> tableColumnDepartmentName;
+    @FXML private TableColumn<Department,String> tableColumnDepartmentDoneWorks;
+    @FXML private TableColumn<Department,String> tableColumnDepartmentAllWorks;
+    @FXML private Label labelDepartmentDoneWork;
+    @FXML private Label labelDepartmentNotDoneWork;
+    @FXML private Label labelDepartmentAll;
+
+    private Map<Department,List<Integer>> departmentStatisticMap;
+    private ReportYear reportYear;
+    private Integer month;
+
+    public void initialize() {
+        reportYear = userSession.getCurrentReportYear(userSession.getServerDate().getYear());
+        month = userSession.getServerDate().getMonthValue();
+        departmentStatisticMap = userSession.getDepartmentStatisticForDate(reportYear,month);
+
+        labelDepartmentAll.setText(labelDepartmentAll.getText() + " " + departmentStatisticMap.keySet().size());
+        labelDepartmentDoneWork.setText(labelDepartmentDoneWork.getText() + " " + doneWorkDepartmentCount());
+        labelDepartmentNotDoneWork.setText(labelDepartmentNotDoneWork.getText() + " " + (departmentStatisticMap.keySet().size() - doneWorkDepartmentCount()));
+
+        choiceBoxYear.setItems(FXCollections.observableArrayList(userSession.getReportYearList()));
+        choiceBoxMonth.setItems(FXCollections.observableArrayList(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12)));
+
+        tableColumnDepartmentName.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getName()));
+        tableColumnDepartmentDoneWorks.setCellValueFactory(param -> new SimpleStringProperty(departmentStatisticMap.get(param.getValue()).get(0).toString()));
+        tableColumnDepartmentAllWorks.setCellValueFactory(param -> new SimpleStringProperty(departmentStatisticMap.get(param.getValue()).get(1).toString()));
+
+        choiceBoxMonth.setValue(month);
+        choiceBoxYear.setValue(reportYear);
+        choiceBoxMonth.setOnAction(event -> {
+            departmentStatisticMap = userSession.getDepartmentStatisticForDate(choiceBoxYear.getValue(),choiceBoxMonth.getValue());
+            tableViewDepartment.refresh();
+        });
+        choiceBoxYear.setOnAction(event -> {
+            departmentStatisticMap = userSession.getDepartmentStatisticForDate(choiceBoxYear.getValue(),choiceBoxMonth.getValue());
+            tableViewDepartment.refresh();
+        });
+
+        tableViewDepartment.setItems(FXCollections.observableArrayList(departmentStatisticMap.keySet()));
+    }
+
+    @FXML public void newUser(ActionEvent event) {
+        userSession.addNewUserFrame(event);
+    }
+    @FXML public void newReview(ActionEvent event) {
+        userSession.addNewReviewFrame(event);
+    }
+    @FXML public void changeUser(ActionEvent event) {
+        userSession.changeUserFrame(event);
+    }
+    @FXML public void createWordForChosenDepartment() {
+
+    }
+    @FXML public void createWordForAllDepartment() {
+
+    }
+
+    /**
+     * Class count amount of departments which send all niokrs in current month
+     * @return amount of department with done work
+     * */
+
+    public Integer doneWorkDepartmentCount() {
+        Integer result = 0;
+        for(List<Integer> list : departmentStatisticMap.values()) {
+            if (list.get(0).equals(list.get(1))) result += 1;
+        }
+        return result;
     }
 }
